b1bccba6a2625968a59f465a3e9c82097f76fbdf
Chandra Shekhar <shekharsharma705@gmail.com>
2014-05-07 19:34:20 +0530

    Adding programDetail controller

---
+++ controllers/ProgramDetailsController.php
@@ -0,0 +1,37 @@
+<?php
+
+class ProgramDetailsController extends BaseController {
+
+    const MODULE_KEY = "programDetails";
+
+    public function run(Resource $resource) {
+
+    }
+
+    public function getProgramListById($pid) {
+        $query = "SELECT * FROM ".ProgramDetails_DBTable::DB_TABLE_NAME." WHERE ";
+        $query .= ProgramDetails_DBTable::PROGRAM_ID."='".$pid."' AND ";
+        $query .= ProgramDetails_DBTable::IS_DELETED."= 0";
+        $resultSet = DBManager::executeQuery($query, array(), true);
+        return current($resultSet);
+    }
+
+    public function deleteProgram($pid) {
+        $programInfo = $this->getProgramListById($pid);
+        $fileToUnlink = Configuration::get('CODE_BASE_DIR') .
+        $programInfo[ProgramDetails_DBTable::FK_LANGUAGE_ID]."/".
+        $programInfo[ProgramDetails_DBTable::FK_CATEGORY_ID]."/".
+        $programInfo[ProgramDetails_DBTable::STORED_FILE_NAME];
+        if (unlink($fileToUnlink)) {
+            $query = "UPDATE ".ProgramDetails_DBTable::DB_TABLE_NAME." SET ";
+            $query .= ProgramDetails_DBTable::IS_DELETED."= '1' WHERE ";
+            $query .= ProgramDetails_DBTable::PROGRAM_ID."='".$pid."' AND ";
+            $query .= ProgramDetails_DBTable::IS_DELETED."= 0";
+            $resultSet = DBManager::executeQuery($query);
+            return $resultSet;
+        } else {
+            Logger::getLogger()->LogFatal("Unable to delete file => ".$fileToUnlink);
+            return false;
+        }
+    }
+}

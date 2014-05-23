e9418c19784eb2e831816da367efd3927d625b9c
Chandra Shekhar <shekharsharma705@gmail.com>
2014-05-23 16:34:24 +0530

    moving classes to helper dir

--- includes/DBManager.php
+++ includes/DBManager.php
@@ -9,13 +9,14 @@
  */
 class DBManager {
 
-    private function __construct() {}
-    
+    private function __construct() {
+    }
+
     private static $instance;
-    
+
     /**
      * Gives PDO database instance. Singleton implementation
-     * 
+     *
      * @throws PDOException
      * @return PDO
      */
@@ -41,7 +42,7 @@ class DBManager {
     /**
      * Executes given query using PDO prepared statements.
      * returns query output if shouldReturn is true
-     * 
+     *
      * @param string $query
      * @param array|false $bindParams
      * @param boolean $shouldReturn
@@ -80,4 +81,31 @@ class DBManager {
             Logger::getLogger()->LogFatal($e->getMessage());
         }
     }
-}    
+
+    /**
+     * Wrapper method for DB update operation
+     *
+     * @param stirng $table
+     * @param array $columns
+     * @param array $bind
+     * @param array $where [Optional]
+     * @return boolean
+     */
+    public static function update($table, array $columns, array $bind, array $where = array()) {
+        $set = array();
+        foreach ($columns as $name => $value) {
+            $set[] = "`$name` = '$value'";
+        }
+        $query = "UPDATE `$table` SET ";
+        $query .= implode(',', $set);
+        if (!empty($where)) {
+            $query .= ' WHERE ';
+            foreach ($where as $name => $value) {
+                $set[] = "$name = '$value'";
+            }
+            $query .= implode(' AND ', $set);
+        }
+        return DBManager::executeQuery($query, $bind);
+    }
+}
+
--- includes/PreferenceKeys.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class PreferenceKeys {
-    
-    const CODE_EDITOR_THEME = 'code_editor_theme';
-}

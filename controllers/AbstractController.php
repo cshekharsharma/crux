1046f083265ee776bf8162487f15313731c52221
Chandra Shekhar <shekharsharma705@gmail.com>
2014-05-30 12:04:45 +0530

    Adding model

--- controllers/AbstractController.php
+++ controllers/AbstractController.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Abstract Controller, meant to be extended by all controllers in app
- * 
+ *
  * @author Chandra Shekhar <shekharsharma705@gmail.com>
  * @since May 11, 2014
  */
@@ -10,11 +10,29 @@ abstract class AbstractController {
     abstract protected function run(Resource $resource);
 
     /**
+     * Instance of model class
+     * 
+     * @var AbstractModel
+     */
+    protected $model;
+
+    /**
+     * Singleton instance of templating engine Smarty
+     * 
      * @var Smarty
      */
     protected $smarty;
 
     public function __construct() {
         $this->smarty = Utils::getSmarty();
+    }
+    
+    /**
+     * Returns model instance for the controller
+     * 
+     * @return AbstractModel
+     */
+    public function getModel() {
+        return $this->model;
     }
 }
---
+++ models/AbstractModel.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Base class for all the models
+ *
+ * @author Chandra Shekhar Sharma <shekharsharma705@gmail.com>
+ * @since 21 May, 2014
+ */
+abstract class AbstractModel {
+
+}

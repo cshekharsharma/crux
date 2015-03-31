2913a5d76deb150b0acd185c21813734f90015ec
Chandra Shekhar <shekharsharma705@gmail.com>
2015-03-31 19:29:12 +0530

    Adding comment in application bean
    Summary: test commit
    
    Test Plan: none
    
    Reviewers: shekhar
    
    Differential Revision: http://phabricator.local.io/D3

--- includes/Application.php
+++ includes/Application.php
@@ -1,14 +1,19 @@
 <?php
 /**
  * Core Application Class, Super class of all MVC classes.
- * 
+ *
  * @author Chandra Shekhar <shekharsharma705@gmail.com>
  * @package includes
  * @version 1.0.0
  * @since Jun 16, 2014
  */
 abstract class Application {
-    
+
+    /**
+     * Bean data
+     *
+     * @var mixed
+     */
     private static $bean;
 
     public function getBean() {
